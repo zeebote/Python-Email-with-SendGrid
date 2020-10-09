5fb6bbdd957d8f46ac5f1a7e21f783fa13b18213
zeebote <63021919+zeebote@users.noreply.github.com>
2020-10-09 10:31:15 -0700

    Update _send_email.py

--- _send_email.py
+++ _send_email.py
@@ -1,9 +1,16 @@
 import sendgrid
 from sendgrid.helpers.mail import *
 SENDGRID_APIKEY = 'This is your SendGrid API key'
+# You can also setup to get API from your OS env variable
+# un comment next line if you use OS variable for your SendGrid API
+# import os
 
 def _send_mail(subj, body, to_e):
+    # comment out next line if you are using OS variable for your API key    
     sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_APIKEY)
+    # Uncomment this line for using OS variable for API key
+    # sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_APIKEY'))
+    
     # This from address is require verify from SendGrid
     from_email = Email("your from email address")
     to_email = To(to_e)
